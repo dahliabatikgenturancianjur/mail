3545084575cc803b3314deadc8a0dd4f770fb7cd
dahliabatikgenturancianjur <dahliabatikgenturancianjur@gmail.com>
2026-07-25 12:37:32 +0700

    Update _middleware.ts

--- app/routes/_middleware.ts
+++ app/routes/_middleware.ts
@@ -2,19 +2,20 @@
 import { basicAuth } from 'hono/basic-auth'
 import { createMiddleware } from 'hono/factory'
 
-export default createMiddleware(async (c, next) => {
-  const username = c.env.MAILBOX_USERNAME;
-  const password = c.env.MAILBOX_PASSWORD;
+export default [
+  createMiddleware(async (c, next) => {
+    const username = c.env.MAILBOX_USERNAME;
+    const password = c.env.MAILBOX_PASSWORD;
 
-  // Proteksi ganda: Jika lupa mengatur environment variable, kunci seluruh sistem
-  if (!username || !password) {
-    return c.text("Kesalahan Sistem: Username dan Password belum dikonfigurasi di Environment Variables.", 500);
-  }
+    if (!username || !password) {
+      return c.text("Kesalahan Sistem: Username dan Password belum dikonfigurasi di Environment Variables.", 500);
+    }
 
-  const auth = basicAuth({
-    username: username,
-    password: password,
-  });
+    const auth = basicAuth({
+      username: username,
+      password: password,
+    });
 
-  return auth(c, next);
-});
+    return auth(c, next);
+  })
+];
